9c4b05a37756d76af048742b4cd10e8a30dfdb16
Bruno Carletti <brunocarletti182@gmail.com>
2023-10-29 12:48:45 +0000

    v1

--- script.js
+++ script.js
@@ -1,52 +1,98 @@
-//open and close features popup
+//select items
+
 const featuresSelect = document.querySelector(".header__select-features")
 const featuresWindow = document.querySelector(".header__features");
-featuresSelect.addEventListener("click", addFeatures);
+const headerArrowfeature = document.querySelector(".header__arrow_features");
+
+const companySelect = document.querySelector(".header__select-company")
+const companyWindow = document.querySelector(".header__company");
+const headerArrowCompany = document.querySelector(".header__arrow_company");
+
+const pageOverlay = document.querySelector(".page__overlay");
+const burguerSelect = document.querySelector(".header__burguer");
+
+const mobileMenu = document.querySelector(".page__half-screen");
+const mobileClose = document.querySelector(".half__close-button");
+
+const selectFeatureHalf = document.querySelector(".half__select-feature");
+const selectCompanyHalf = document.querySelector(".half__select-company");
+
+const featuresHalf = document.querySelector(".half__hidden-features");
+const companyHalf = document.querySelector(".half__hidden-company");
+
+//functions
+
 function addFeatures() {
   featuresWindow.classList.toggle("header_show");
   companyWindow.classList.remove("header_show")
+  headerArrowfeature.classList.toggle("header__arrow_up");
 }
 
-//open and close company popup
-const companySelect = document.querySelector(".header__select-company")
-const companyWindow = document.querySelector(".header__company");
-companySelect.addEventListener("click", addCompany);
 function addCompany() {
   companyWindow.classList.toggle("header_show");
   featuresWindow.classList.remove("header_show")
+  headerArrowCompany.classList.toggle("header__arrow_up");
 }
 
-//close popups when you click anywhere outside of the window
-document.addEventListener("click", closePopup);
-  
-  function closePopup(event){
+function closePopup(event){
   if (!featuresSelect.contains(event.target) && !featuresWindow.contains(event.target)) {
     featuresWindow.classList.remove("header_show");
+    headerArrowfeature.classList.remove("header__arrow_up");
   }
 
   if (!companySelect.contains(event.target) && !companyWindow.contains(event.target)) {
     companyWindow.classList.remove("header_show");
+    headerArrowCompany.classList.remove("header__arrow_up");
   }
 }
 
-const burguerSelect = document.querySelector(".header__burguer");
-const mobileMenu = document.querySelector(".page__half-screen");
-const pageOverlay = document.querySelector(".page__overlay");
-
-const mobileClose = document.querySelector(".half__close-button");
-
 function addMobileMenu() {
-  mobileMenu.classList.toggle("page__half-screen_show");
+  mobileMenu.classList.add("page__half-screen_show");
   pageOverlay.classList.add("page__overlay_show");
 }
 
 function closeMobileMenu() {
   mobileMenu.classList.remove("page__half-screen_show");
   pageOverlay.classList.remove("page__overlay_show");
+  companyHalf.classList.remove("half__hidden_show");
+  featuresHalf.classList.remove("half__hidden_show");
 }
 
+function handleScreenSizeChange() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= 719) {
+    // Hide the mobile menu and overlay on larger screens
+    mobileMenu.classList.remove("page__half-screen_show");
+    pageOverlay.classList.remove("page__overlay_show");
+  }
+}
+
+function addFeaturesHalf() {  
+  featuresHalf.classList.toggle("half__hidden_show");
+  companyHalf.classList.remove("half__hidden_show");
+}
+
+function addCompanyHalf() {
+  companyHalf.classList.toggle("half__hidden_show");
+  featuresHalf.classList.remove("half__hidden_show");
+}
+
+//event listeners
+featuresSelect.addEventListener("click", addFeatures);
+companySelect.addEventListener("click", addCompany);
+document.addEventListener("click", closePopup);
 mobileClose.addEventListener("click", closeMobileMenu);
 burguerSelect.addEventListener("click", addMobileMenu);
+window.addEventListener("resize", handleScreenSizeChange);
+selectFeatureHalf.addEventListener("click", addFeaturesHalf);
+selectCompanyHalf.addEventListener("click", addCompanyHalf);
+
+//call functions
+handleScreenSizeChange();
+
+
+
+
 
 
 
